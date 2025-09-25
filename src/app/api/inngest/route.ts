82638805e788e62363b97a43152915006b873283
Yakuphan Küçükkesim <yakuphann@icloud.com>
2025-09-25 18:22:04 +0300

    Fix: Inngest app name ve Vercel uyumu

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,14 +1,11 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { processVideo, testFunction, simpleTest, minimalTest } from "~/inngest/functions";
+import { processVideo } from "~/inngest/functions";
 
 // Create an API that serves Inngest functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    processVideo,
-    testFunction,
-    simpleTest,
-    minimalTest
+    processVideo
   ],
 });
